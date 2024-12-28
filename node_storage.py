2149f9692a0035c1c19c238389b226944ac56713
MatthieuHR <matt.hiessler@gmail.com>
2024-12-28 18:40:32 +0100

    Plus de commentaires

--- node_storage.py
+++ node_storage.py
@@ -5,7 +5,7 @@ class Tekko_Node_Storage:
     def __init__(self):
         self.map = {}
     
-
+    ## Renvoie une node en fonction de l'état et du joueur, si la node n'existe pas elle la crée
     def get_node(self, state, player):
         key = str(player) + " " + str(state)
         if key in self.map:
@@ -14,7 +14,8 @@ class Tekko_Node_Storage:
             rtn = teeko_node.Teeko_node(state,player)
             self.map[key] = rtn
             return rtn
-        
+    
+    ## Renvoie une liste de node terminale, c'est à dire les nodes qui sont gagantes
     def get_terminal_node(self):
         list = []
 
